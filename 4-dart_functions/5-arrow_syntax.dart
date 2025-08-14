f82ceb4c707b3643bb1a5c9cad23302f1c6403a7
Maurlcio <thecoolermess@gmail.com>
2025-08-14 12:21:30 -0300

    fixed exercise that had erroneously been overwritten with the previous exercise

--- 4-dart_functions/5-arrow_syntax.dart
+++ 4-dart_functions/5-arrow_syntax.dart
@@ -1,18 +1,5 @@
-void printMultiple(int number, [int max = 5]){
-  int i = 0;
-  int multiple = number;
-  List multiples = [];
-
-  while (i != max){
-    multiples.add(multiple);
-    multiple += number;
-    i += 1;
-  }
-
-  print(multiples.join(" "));
-}
+int square(int x) => x * x;
 
 void main(){
-  printMultiple(3);
-  printMultiple(4, 3);
+  print(square(6));
 }
